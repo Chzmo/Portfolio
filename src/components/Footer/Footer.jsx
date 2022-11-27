a5879b528c1e3872d99bf695f73b87eefd7cb6b2
chzmo <chisomomoyo10@gmail.com>
2022-11-27 14:10:29 +0200

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './Footer.css'
+
 function Footer() {
   return (
     <div className='footer'>
@@ -18,10 +20,34 @@ function Footer() {
           </div>
         </div>
         <div className="footer__top-nav">
-          <nav></nav>
+          <nav>
+            <ul role="list" className="footer__top-nav-list">
+              <li className="footer__top-nav-list-item">
+                <a href="#Home" className="footer__top-nav-link">Home</a>
+              </li>
+              <li className="footer__top-nav-list-item">
+                <a href="#Services" className="footer__top-nav-link">Services</a>
+              </li>
+              <li className="footer__top-nav-list-item">
+                <a href="#" className="footer__top-nav-link">About Me</a>
+              </li>
+              <li className="footer__top-nav-list-item">
+                <a href="#Testimonials" className="footer__top-nav-link">Testimonials</a>
+              </li>
+              <li className="footer__top-nav-list-item">
+                <a href="#Recent-Work" className="footer__top-nav-link">Recent Work</a>
+              </li>
+              <li className="footer__top-nav-list-item">
+                <a href="#FAQs" className="footer__top-nav-link">FAQs</a>
+                <a href="#Pricing" className="footer__top-nav-link">Contact</a>
+              </li>
+          </ul>
+          </nav>
         </div>
       </div>
-      <div className="footer__bottom"></div>
+      <div className="footer__bottom">
+        fsdfsdfs
+      </div>
     </div>
   )
 }
