55fe281bf800f804a3558296b7920ce5039f637d
Chzmo <chisomomoyo10@gmail.com>
2022-12-10 09:21:22 +0200

    working on Single page Blog

--- src/container/SingleBlog/SingleBlog.jsx
+++ src/container/SingleBlog/SingleBlog.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import {CiHeart} from 'react-icons/ci'
+import {RiShareLine} from 'react-icons/ri'
+import {BiMessageRoundedDetail} from 'react-icons/bi'
 
 import NavBar from '../../components/NavBar/NavBar'
 import Footer from '../../components/Footer/Footer'
@@ -23,17 +26,19 @@ function SingleBlog() {
           <div className= "singleBlog__main-img">
             {/* background image */}
           </div>
-          <div className="singleBlog__main">
+          <div className="singleBlog__blog">
             <div className="socials">
               <div className="socials__date"><p>2 years ago</p></div>
               <div className="socials__links">
-                <div className="socials__links-like">1K</div>
-                <div className="socials__links-comment">38</div>
-                <div className="socials__links-share">5</div>
+                <div className="socials__links-like"><a href=""><CiHeart className="social-icon"/> <span>30K</span></a></div>
+                <div className="socials__links-comment"><a href=""><BiMessageRoundedDetail className="social-icon"/> <span>390</span></a></div>
+                <div className="socials__links-share"><a href=""><RiShareLine className="social-icon"/> <span>Share</span></a></div>
               </div>
             </div>
             <div className="blog">
-              <div className="blog__post"></div>
+              <div className="blog__post">
+                vccxvxcv
+              </div>
               <div className="blog__comments"></div>
             </div>
             <div className="related">
