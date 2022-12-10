b1ddb8d47da15d1ee7bdf137e966f758f9c24e5c
Chzmo <chisomomoyo10@gmail.com>
2022-12-10 08:19:32 +0200

    working on Single page Blog

--- src/container/SingleBlog/SingleBlog.jsx
+++ src/container/SingleBlog/SingleBlog.jsx
@@ -9,20 +9,41 @@ function SingleBlog() {
   return (
     <>
       <NavBar />
-      <div className='container'>Blog</div>
-      <div className='container'>Blog</div>
-      <div className='container'>Blog</div>
-      <div className='container'>Blog</div>
-      <div className='container'>Blog</div>
-      <div className='container'>Blog</div>
-      <div className='container'>Blog</div>
-      <div className='container'>Blog</div>
-      <div className='container'>Blog</div>
-      <div className='container'>Blog</div>
-      <div className='container'>Blog</div>
-      <div className='container'>Blog</div>
-      <div className='container'>Blog</div>
-      <div className='container'>Blog</div>
+      <div className='singleBlog'>
+        <div className="singleBlog__header">
+          <h2>Shopify Website Design and Development for Organic Products</h2>
+          <div className="singleBlog__header-related">
+            <button>Landing Page</button>
+            <button>Shopify</button>
+            <button>Wordpress</button>
+          </div>
+        </div>
+        <p>Home {'/'} Blog {'/'} Post</p>
+        <div className="singleBlog__main">
+          <div className= "singleBlog__main-img">
+            {/* background image */}
+          </div>
+          <div className="singleBlog__main">
+            <div className="socials">
+              <div className="socials__date"><p>2 years ago</p></div>
+              <div className="socials__links">
+                <div className="socials__links-like">1K</div>
+                <div className="socials__links-comment">38</div>
+                <div className="socials__links-share">5</div>
+              </div>
+            </div>
+            <div className="blog">
+              <div className="blog__post"></div>
+              <div className="blog__comments"></div>
+            </div>
+            <div className="related">
+              <h2>Related Blog Posts</h2>
+              <div className="related__img"></div>
+              <div className="related__title"></div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="footer__wrapper">
         <Footer />
       </div>
