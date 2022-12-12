02645691619381bbf8a7591e08279d9fde2d3176
Chzmo <chisomomoyo10@gmail.com>
2022-12-12 15:26:16 +0200

    mobile responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './NavBar.css'
 
+
+
 function NavBar() {
+  
+  const [mobileNav, setMobileNav] = useState(false)
+
+  function responsiveNav(){
+    !mobileNav ? setMobileNav(true): setMobileNav(false) ;
+  }
+  
   return (
     <nav className='navbar'>
       <div className="navbar__logo">
         <a href="/">CH<span>ZMO</span></a>
       </div>
       <div className="navbar__links">
-        <ul className="navbar__links-ul">
+        <ul className={mobileNav? "mobile": "navbar__links-ul"}>
           <li className="navbar__links-ul_li"><a className="navbar__links-ul_li-a"href="/#Testimonials">Testimonials</a></li>
           <li className="navbar__links-ul_li"><a className="navbar__links-ul_li-a"href="/Work">Latest Work</a></li>
           <li className="navbar__links-ul_li"><a className="navbar__links-ul_li-a"href="/Blog">Blog</a></li>
           <li className="navbar__links-ul_li"><a className="navbar__links-ul_li-a"href="/Contact">Contact</a></li>
           <li className="navbar__links-ul_li"><a className="navbar__links-ul_li-a"href="/Login">Login</a></li>
         </ul>
+        <div onClick={responsiveNav} className='button'>
+          <div></div><div></div><div></div>
+        </div>
       </div>
     </nav>
   )
