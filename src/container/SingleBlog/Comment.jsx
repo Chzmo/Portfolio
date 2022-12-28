2e9e63eda369ff25763b83c48cc4e592f9712417
Chzmo <chisomomoyo10@gmail.com>
2022-12-28 17:46:43 +0200

    fixed key issues

--- src/container/SingleBlog/Comment.jsx
+++ src/container/SingleBlog/Comment.jsx
@@ -39,64 +39,65 @@ export function CommentField(props){
 
 function Comment(props){
 
-  return ( 
+  return (
     <>
-    {props.items.map((comment)=>{
-      return(
-        <>
-          <div className="comments__main" key={comment.id}>
-            <div className="comments__main-profile">
-              <img src={profileImg} alt="profile" />
-            </div>
-            <div className="comments__main-content">
-              <div className="comments__main-content_top">
-                <h3>{comment.user.username}</h3> <p>3 Months ago</p>
-              </div>
-              <div className="comments__main-content_message">
-                <p>{comment.content}</p>
-              </div>
-              <div className="comments__main-content_bottom">
-                <div className="content_bottom-social">
-                  <button><CiHeart className="social-icon"/><span>{comment.likes}</span></button>
-                </div>
-                <div className="content_bottom-reply"><button><BsReplyAll className="social-icon"/><span>Reply</span></button></div>
-              </div>
-            </div>
+      {props.items.map((comment) => {
+  return (
+    <div key={comment.id}>
+      <div className="comments__main">
+        <div className="comments__main-profile">
+          <img src={profileImg} alt="profile" />
+        </div>
+        <div className="comments__main-content">
+          <div className="comments__main-content_top">
+            <h3>{comment.user.username}</h3> <p>3 Months ago</p>
           </div>
-          {
-            comment?.replies &&(
-              comment.replies.map(reply =>{
-                return(
-                  <div className="reply" key={reply.id}>
-                    <div className="comments__main">
-                      <div className="comments__main-profile">
-                        <img src={profileImg} alt="profile" />
+          <div className="comments__main-content_message">
+            <p>{comment.content}</p>
+          </div>
+          <div className="comments__main-content_bottom">
+            <div className="content_bottom-social">
+              <button><CiHeart className="social-icon"/><span>{comment.likes}</span></button>
+            </div>
+            <div className="content_bottom-reply"><button onClick={hundleReply}><BsReplyAll className="social-icon"/><span>Reply</span></button></div>
+          </div>
+        </div>
+      </div>
+      {
+        comment?.replies &&(
+          comment.replies.map((reply) => {
+            return (
+              <div className="reply" key={reply.id}>
+                <div className="comments__main">
+                  <div className="comments__main-profile">
+                    <img src={profileImg} alt="profile" />
+                  </div>
+                  <div className="comments__main-content">
+                    <div className="comments__main-content_top">
+                      <h3>{reply.user.username}</h3> <p>3 Months ago</p>
+                    </div>
+                    <div className="comments__main-content_message">
+                      <p>{reply.content}</p>
+                    </div>
+                    <div className="comments__main-content_bottom">
+                      <div className="content_bottom-social">
+                        <button><CiHeart className="social-icon"/><span>{reply.likes}</span></button>
                       </div>
-                      <div className="comments__main-content">
-                        <div className="comments__main-content_top">
-                          <h3>{reply.user.username}</h3> <p>3 Months ago</p>
-                        </div>
-                        <div className="comments__main-content_message">
-                          <p>{reply.content}</p>
-                        </div>
-                        <div className="comments__main-content_bottom">
-                          <div className="content_bottom-social">
-                            <button><CiHeart className="social-icon"/><span>{reply.likes}</span></button>
-                          </div>
-                          <div className="content_bottom-reply"><button onClick={hundleReply}><BsReplyAll className="social-icon" /><span>Reply</span></button></div>
-                        </div>
-                      </div>
+                      <div className="content_bottom-reply"><button onClick={hundleReply}><BsReplyAll className="social-icon" /><span>Reply</span></button></div>
                     </div>
                   </div>
-                )
-              })
+                </div>
+              </div>
             )
-          }
-        </>
-      )
-    })}
-    </>
+          })
+        )
+      }
+    </div>
   );
+})}
+
+    </>
+  )
 }
 
 export default Comment
