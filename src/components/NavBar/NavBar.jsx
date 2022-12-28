c813f2c243eac780332ffe0ac63515997c109869
Chzmo <chisomomoyo10@gmail.com>
2022-12-28 14:46:26 +0200

    fixing links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -32,6 +32,7 @@ function NavBar() {
       </div>
       <div className="navbar__links">
         <ul className={mobileNav? "mobile": "navbar__links-ul"}>
+          <li className="navbar__links-ul_li"><HashLink to="/#About" className="footer__top-nav-link" smooth>About Me</HashLink></li>
           <li className="navbar__links-ul_li"><HashLink className="navbar__links-ul_li-a" to="/#Testimonials">Testimonials</HashLink></li>
           <li className="navbar__links-ul_li"><Link className="navbar__links-ul_li-a" to="/Work">Latest Work</Link></li>
           <li className="navbar__links-ul_li"><Link className="navbar__links-ul_li-a" to="/Blog">Blog</Link></li>
