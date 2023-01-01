d0900cdb9c62fb1ec573776c7961f583a5b45942
Chzmo <chisomomoyo10@gmail.com>
2023-01-01 15:27:39 +0200

    working on auth0

--- src/auth/auth0-provider-with-history.jsx
+++ src/auth/auth0-provider-with-history.jsx
@@ -4,10 +4,9 @@ import { useNavigate } from 'react-router-dom';
 
 export const Auth0ProviderWithHistory = ({ children }) => {
 
-  const domain = process.env?.VITE_AUTH0_DOMAIN;
-  const clientId = process.env?.VITE_AUTH0_CLIENT_ID;
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = window.location.redirectUri;
-  
   const onRedirectCallback = (appState) => {
     useNavigate().push(appState?.returnTo || window.location.pathname);
   };
