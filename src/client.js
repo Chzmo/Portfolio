e3d2f64f4e6433d1bfb5bac24230e7ab8622ca1a
chzmo <chisomomoyo10@gmail.com>
2023-01-10 12:06:41 +0200

    connecting to database

--- src/client.js
+++ src/client.js
@@ -2,13 +2,13 @@ import sanityClient from '@sanity/client';
 import imageUrlBulder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectId: "tv4z50ix",
+    projectId: import.meta.env.VITE_APP_SANITY_PROJECT_ID,
     dataset:'production',
     apiVersion: '2021-10-21',
     useCdn:false,
     token: import.meta.env.VITE_APP_SANITY_API_TOKEN, 
     ignoreBrowserTokenWarning: true
 });
-// console.log(import.meta.env.VITE_APP_SANITY_PROJECT_ID, import.meta.env.VITE_APP_SANITY_API_TOKEN)
+
 const builder = imageUrlBulder(client);
 export const urlFor = (source) => builder.image(source);
