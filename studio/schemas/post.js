e4df4c98767b735959e5ba901d57dccd67231a89
chzmo <chisomomoyo10@gmail.com>
2023-01-22 20:08:42 +0200

    working singleblog

--- studio/schemas/post.js
+++ studio/schemas/post.js
@@ -4,6 +4,11 @@ export default {
     type: 'document',
     fields:[
         {
+            name: '_createdAt',
+            title: 'Created At',
+            type: 'date'
+        },
+        {
             name: 'title',
             title: 'Title',
             type: 'string'
